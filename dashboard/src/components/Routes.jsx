adb8de8c4c8bb611061f1893428b13feebf90dca
zakari64 <74094535+zakari64@users.noreply.github.com>
2021-12-27 20:14:28 +0100

    added table

--- dashboard/src/components/Routes.jsx
+++ dashboard/src/components/Routes.jsx
@@ -4,13 +4,14 @@ import { Route, Switch } from 'react-router-dom'
 
 import Dashboard from '../pages/Dashboard'
 import Customers from '../pages/Customers'
-import Stats from '../pages/Stats'
+import Operations from '../pages/Operations'
 
 const Routes = () => {
     return (
         <Switch>
             <Route path='/layout/dashboard' component={Dashboard}/>
             <Route path='/layout/customers' component={Customers}/>
+            <Route path='/layout/operations' component={Operations} />
         </Switch>
     )
 }
---
+++ dashboard/src/pages/Operations.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import AddBox from "@material-ui/icons/AddBox";
+import MaterialTable from 'material-table'
+import ArrowDownward from "@material-ui/icons/ArrowDownward";
+import { forwardRef } from 'react';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+
+
+function Operations() {
+   
+    return (
+        <MaterialTable
+          title="Simple Action Preview"
+          columns={[
+            { title: 'Name', field: 'name' },
+            { title: 'Surname', field: 'surname' },
+            { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+            {
+              title: 'Birth Place',
+              field: 'birthCity',
+              lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+            },
+          ]}
+          data={[
+            { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+            { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+          ]}        
+          actions={[
+            {
+              icon: 'save',
+              tooltip: 'Save User',
+              onClick: (event, rowData) => alert("You saved " + rowData.name)
+            }
+          ]}
+        />
+      )
+  }
+
+export default Operations
